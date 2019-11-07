89473e579a879aaf7b4e0f5b2fd09950e55d7908
joony <bestheroz@gmail.com>
2019-11-07 22:26:03 +0900

    exclusion jackson

--- src/main/java/com/github/bestheroz/standard/context/web/WebConfig.java
+++ src/main/java/com/github/bestheroz/standard/context/web/WebConfig.java
@@ -1,13 +1,20 @@
 package com.github.bestheroz.standard.context.web;
 
 import com.github.bestheroz.standard.common.interceptor.Interceptor;
+import com.github.bestheroz.standard.common.util.MyMapperUtils;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -31,6 +38,21 @@ public class WebConfig implements WebMvcConfigurer {
         resolver.setSuffix(".jsp");
         resolver.setViewClass(JstlView.class);
         registry.viewResolver(resolver);
+    }
+
+    @Override
+    public void configureMessageConverters(final List<HttpMessageConverter<?>> converters) {
+        converters.clear();
+
+        final GsonHttpMessageConverter gsonHttpMessageConverter = new GsonHttpMessageConverter();
+        gsonHttpMessageConverter.setGson(MyMapperUtils.getGsonObject());
+        converters.add(gsonHttpMessageConverter);
+
+        final StringHttpMessageConverter stringHttpMessageConverter = new StringHttpMessageConverter();
+        stringHttpMessageConverter.setDefaultCharset(StandardCharsets.UTF_8);
+        converters.add(stringHttpMessageConverter);
+
+        WebMvcConfigurer.super.configureMessageConverters(converters);
     }
 
 }
